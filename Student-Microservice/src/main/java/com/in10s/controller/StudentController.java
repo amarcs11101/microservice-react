bf2bb2cbb40d28e5ffadc36265e1e023f987ba94
Abhishek Amar <abhishek.a@intense.in>
2023-12-15 12:32:08 +0530

    +

--- Student-Microservice/src/main/java/com/in10s/controller/StudentController.java
+++ Student-Microservice/src/main/java/com/in10s/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.in10s.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.in10s.request.CreateStudentRequest;
 import com.in10s.service.StudentService;
-
+/***
+ * @author Abhishek Amar
+ */
 @RestController
 @RequestMapping("/api/student")
 public class StudentController {
@@ -20,7 +25,10 @@ public class StudentController {
 	
 	@PostMapping("/create")
 	public  ResponseEntity<?> createStudent (@RequestBody CreateStudentRequest createStudentRequest) {
-		return new ResponseEntity<String>("Saved",HttpStatus.OK);
+		Map<String,Object> data = new HashMap<>();
+		data.put("message", "Student saved successfully , from student microservice. ");
+		data.put("data", createStudentRequest);
+		return new ResponseEntity<Map<String,Object>>(data,HttpStatus.OK);
 	}
 	 
 	
