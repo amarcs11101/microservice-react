5474f70ae2652fbf6f3c31d063651c997384c963
Abhishek Amar <abhishek.a@intense.in>
2023-12-15 13:06:03 +0530

    +

--- react/src/App.js
+++ react/src/App.js
@@ -1,14 +1,16 @@
 import axios from 'axios'
 export default function Button() {
   const studentApi = "http://localhost:9191/api/student/create";
-  const addressApi =  "http://localhost:9191/api/address/create";
+  const addressApi = "http://localhost:9191/api/address/create"; 
   function apiI() {
     console.log('student api call from api gateway'); 
       axios.post(studentApi, { firstName: "Amar", lastName: "Abhishek", email: "abhishek.a@intense.in", addressId: 0
       })
         .then((res) => {
           console.log(":: student api response ",res)
-          alert(res.data.message)
+         // alert(res.data.message)
+          document.getElementById("dataid").innerHTML=res.data.message
+          document.getElementById("data").innerHTML=JSON.stringify(res.data)
         })
         .catch((error) => {
           console.log(error); 
@@ -20,7 +22,9 @@ export default function Button() {
       axios.post(addressApi, { street: "xyz", city: "abv"  })
         .then((res) => {
           console.log(":: address api response ",res)
-          alert(res.data.message)
+        //  alert(res.data.message)
+          document.getElementById("dataid").innerHTML=res.data.message
+          document.getElementById("data").innerHTML=JSON.stringify(res.data)
         })
         .catch((error) => {
           console.log(error); 
@@ -32,6 +36,9 @@ export default function Button() {
        CLICK MICROSERVICE I
     </button><button onClick={apiII}>
      CLICK MICROSERVICE II
-      </button></center></>
+      </button></center>
+      <center><h6 id='dataid' >Welcome to microservice</h6> </center>
+      <center id='data'></center>
+      </>
   );
 }
